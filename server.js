39d0ffc275d0c1002f03385782fdc58fc91875fe
John Siwicki <johnsiwicki@Johns-iMac.local>
2016-08-30 13:31:27 -0400

    make it better

--- server.js
+++ server.js
@@ -77,6 +77,6 @@ fs.writeFileSync("borders.html", html);
 	res.sendFile(__dirname + '/done.html');
 });
 
-app.listen(3000, function () {  
-    console.log('Example app listening on port 3000!')
-})
+// app.listen(3000, function () {  
+//     console.log('Example app listening on port 3000!')
+// })
