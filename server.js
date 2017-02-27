485d1747d481bb4aeffecbd470fd512990c098ef
John Siwicki <johnsiwicki@gmail.com>
2017-02-27 09:11:54 -0500

    moved alt tags into the image tag

--- server.js
+++ server.js
@@ -47,83 +47,83 @@ var servicesList = req.body.services;
 // // all of our services data
 var list = [{
     sName:"Basement Finishing",
-    sFile: '<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/basement-finishing.html" title="Basement Finishing in [territory], [major cities 3]" alt="Basement Finishing in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Basement Finishing</p></a><p>Gain extra living space in your home with a custom-designed finished basement.</p></div> </div>'
+    sFile: '<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/basement-finishing.html" title="Basement Finishing in [territory], [major cities 3]"><img alt="Basement Finishing in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Basement Finishing</p></a><p>Gain extra living space in your home with a custom-designed finished basement.</p></div> </div>'
 },
 {
     sName:'Basement Waterproofing',
-    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/basement-waterproofing.html" title="Basement Waterproofing in [territory], [major cities 3]" alt="Basement Waterproofing in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Basement Waterproofing</p></a><p>If your basement is wet, cracked, or smells musty – we can help.</p></div> </div>'
+    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/basement-waterproofing.html" title="Basement Waterproofing in [territory], [major cities 3]"><img  alt="Basement Waterproofing in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Basement Waterproofing</p></a><p>If your basement is wet, cracked, or smells musty – we can help.</p></div> </div>'
 },
 {
     sName:"Crawl Space Repair",
-    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/crawl-space-repair.html" title="Crawl Space Repair in [territory], [major cities 3]" alt="Crawl Space Repair in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Crawl Space</p></a><p>Learn why more homeowners choose our patented CleanSpace system.</p></div> </div>'
+    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/crawl-space-repair.html" title="Crawl Space Repair in [territory], [major cities 3]"><img alt="Crawl Space Repair in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Crawl Space</p></a><p>Learn why more homeowners choose our patented CleanSpace system.</p></div> </div>'
 },
 {
     sName:"Decks",
-    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/decks-patios.html" title="Decks in [territory], [major cities 3]" alt="Decks in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Decks</p></a><p>We can restore your old wood deck or build a brand new one with quality materials.</p></div> </div>'
+    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/decks-patios.html" title="Decks in [territory], [major cities 3]"><img alt="Decks in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Decks</p></a><p>We can restore your old wood deck or build a brand new one with quality materials.</p></div> </div>'
 },
 {
     sName:"Fire Damage Restoration",
-    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/disaster-restoration/fire-damage.html" title="Fire Damage Restoration in [territory], [major cities 3]" alt="Fire Damage Restoration in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Fire Damage Restoration</p></a><p>After a fire, we can repair water, smoke and soot damage and restore your property.</p></div> </div>'
+    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/disaster-restoration/fire-damage.html" title="Fire Damage Restoration in [territory], [major cities 3]"><img alt="Fire Damage Restoration in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Fire Damage Restoration</p></a><p>After a fire, we can repair water, smoke and soot damage and restore your property.</p></div> </div>'
 },
 {
     sName:"Flood Damage",
-    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/disaster-restoration/water-damage/flooded-basement.html" title="Flood Damage in [territory], [major cities 3]" alt="Flood Damage in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Flood Damage</p></a><p>We have the expertise and equipment to handle any storm and flood damage job.</p></div> </div>'
+    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/disaster-restoration/water-damage/flooded-basement.html" title="Flood Damage in [territory], [major cities 3]"><img alt="Flood Damage in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Flood Damage</p></a><p>We have the expertise and equipment to handle any storm and flood damage job.</p></div> </div>'
 },
 {
     sName:"Gutters",
-    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/gutters.html" title="Gutters in [territory], [major cities 3]" alt="Gutters in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Gutters</p></a><p>If you need new gutters or leaf protection, we offer custom solutions and installation.</p></div> </div>'
+    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/gutters.html" title="Gutters in [territory], [major cities 3]"><img alt="Gutters in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Gutters</p></a><p>If you need new gutters or leaf protection, we offer custom solutions and installation.</p></div> </div>'
 },
 {
     sName:"Insulation",
-    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/insulation.html" title="Insulation in [territory], [major cities 3]" alt="Insulation in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Insulation</p></a><p>We offer a variety of insulation options that can make a difference in your home\'s comfort.</p></div> </div>'
+    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/insulation.html" title="Insulation in [territory], [major cities 3]"><img alt="Insulation in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Insulation</p></a><p>We offer a variety of insulation options that can make a difference in your home\'s comfort.</p></div> </div>'
 },
 {
     sName:"Mold Removal",
-    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/mold-removal.html" title="Mold Removal in [territory], [major cities 3]" alt="Mold Removal in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Mold Removal</p></a><p>We use advanced mold removal products and techniques to protect your family and home.</p></div> </div>'
+    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/mold-removal.html" title="Mold Removal in [territory], [major cities 3]"><img alt="Mold Removal in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Mold Removal</p></a><p>We use advanced mold removal products and techniques to protect your family and home.</p></div> </div>'
 },
 {
     sName:"Pest Control",
-    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/pest-control.html" title="Pest Control in [territory], [major cities 3]" alt="Pest Control in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Pest Control</p></a><p>We have the products and expertise to rid your home of unwanted pests and insects.</p></div> </div>'
+    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/pest-control.html" title="Pest Control in [territory], [major cities 3]"><img alt="Pest Control in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Pest Control</p></a><p>We have the products and expertise to rid your home of unwanted pests and insects.</p></div> </div>'
 },
 {
     sName:"Roofing",
-    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/roof-repair.html" title="Roofing in [territory], [major cities 3]" alt="Roofing in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Roofing</p></a><p>Keep your home protected year round with our quality roofing systems and services.</p></div> </div>'
+    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/roof-repair.html" title="Roofing in [territory], [major cities 3]"><img alt="Roofing in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Roofing</p></a><p>Keep your home protected year round with our quality roofing systems and services.</p></div> </div>'
 },
 {
     sName:"Roof Repair",
-    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/roof-repair.html" title="Roof Repair in [territory], [major cities 3]" alt="Roof Repair in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Roof Repair</p></a><p>We fix roof leaks and roofing problems with prompt, expert service and quality products.</p></div> </div>'
+    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/roof-repair.html" title="Roof Repair in [territory], [major cities 3]"><img alt="Roof Repair in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Roof Repair</p></a><p>We fix roof leaks and roofing problems with prompt, expert service and quality products.</p></div> </div>'
 },
 {
     sName:"Roof Replacement",
-    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/roof-replacement.html" title="Roof Replacement in [territory], [major cities 3]" alt="Roof Replacement in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Roof Replacement</p></a><p>When it\'s time for a new roof, we provide quality products and worry-free installation.</p></div> </div>'
+    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/roof-replacement.html" title="Roof Replacement in [territory], [major cities 3]"><img alt="Roof Replacement in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Roof Replacement</p></a><p>When it\'s time for a new roof, we provide quality products and worry-free installation.</p></div> </div>'
 },
 {
     sName:"Remodeling",
-    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/remodeling.html" title="Remodeling in [territory], [major cities 3]" alt="Remodeling in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Remodeling</p></a><p>Turn your home remodeling ideas into a reality with our expert design-build services.</p></div> </div>'
+    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/remodeling.html" title="Remodeling in [territory], [major cities 3]"><img alt="Remodeling in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Remodeling</p></a><p>Turn your home remodeling ideas into a reality with our expert design-build services.</p></div> </div>'
 },
 {
     sName:"Replacement Windows",
-    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/windows-doors/replacement-windows.html" title="Replacement Windows in [territory], [major cities 3]" alt="Replacement Windows in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Replacement Windows</p></a><p>Improve your home\'s comfort and curb appeal with beautiful, energy-efficient windows.</p></div> </div>'
+    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/windows-doors/replacement-windows.html" title="Replacement Windows in [territory], [major cities 3]"><img alt="Replacement Windows in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Replacement Windows</p></a><p>Improve your home\'s comfort and curb appeal with beautiful, energy-efficient windows.</p></div> </div>'
 },
 {
     sName:"Siding",
-    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/siding.html" title="Siding in [territory], [major cities 3]" alt="Siding in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Siding</p></a><p>Upgrade your home\'s exterior with our durable siding products and professional installation.</p></div> </div>'
+    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/siding.html" title="Siding in [territory], [major cities 3]" ><img alt="Siding in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Siding</p></a><p>Upgrade your home\'s exterior with our durable siding products and professional installation.</p></div> </div>'
 },
 {
     sName:"Sinkhole Repair",
-    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/sinkhole-repair.html" title="Sinkhole Repair in [territory], [major cities 3]" alt="Sinkhole Repair in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Sinkhole Repair</p></a><p>We can stabilize your foundation or slab to help prevent sinkhole damage.</p></div> </div>'
+    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/sinkhole-repair.html" title="Sinkhole Repair in [territory], [major cities 3]"><img alt="Sinkhole Repair in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Sinkhole Repair</p></a><p>We can stabilize your foundation or slab to help prevent sinkhole damage.</p></div> </div>'
 },
 {
     sName:"Sunrooms",
-    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/sunrooms.html" title="Sunrooms in [territory], [major cities 3]" alt="Sunrooms in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Sunrooms</p></a><p>Enjoy your outdoor living space in any weather with a custom sunroom addition.</p></div> </div>'
+    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/sunrooms.html" title="Sunrooms in [territory], [major cities 3]"><img alt="Sunrooms in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Sunrooms</p></a><p>Enjoy your outdoor living space in any weather with a custom sunroom addition.</p></div> </div>'
 },
 {
     sName:"Wind Damage Restoration",
-    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/disaster-restoration/water-damage.html" title="Wind Damage Restoration in [territory], [major cities 3]" alt="Wind Damage Restoration in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Wind Damage Restoration</p></a><p>When strong winds cause damage to your home or property, trust our experts for fast response.</p></div> </div>'
+    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/disaster-restoration/water-damage.html" title="Wind Damage Restoration in [territory], [major cities 3]"><img alt="Wind Damage Restoration in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Wind Damage Restoration</p></a><p>When strong winds cause damage to your home or property, trust our experts for fast response.</p></div> </div>'
 },
 {
     sName:"Windows & Doors",
-    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/windows-doors.html" title="Windows &amp; Doors in [territory], [major cities 3]" alt="Windows &amp; Doors in [territory], [major cities 3]"><img src="http://placehold.it/150x150"><p class="service-title">Windows &amp; Doors</p></a><p>We install energy-efficient replacement windows and doors for every budget.</p></div> </div>'
+    sFile:'<div class="small-6 medium-3 columns"> <div class="service-item"><a href="/windows-doors.html" title="Windows &amp; Doors in [territory], [major cities 3]"><img  alt="Windows &amp; Doors in [territory], [major cities 3]" src="http://placehold.it/150x150"><p class="service-title">Windows &amp; Doors</p></a><p>We install energy-efficient replacement windows and doors for every budget.</p></div> </div>'
 }
 ];
 
